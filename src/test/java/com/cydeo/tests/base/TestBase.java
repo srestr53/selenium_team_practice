501ad542b5249c6b62b23c2f32a3683208672b8b
srestr53 <sangil9091@gmail.com>
2023-12-20 07:42:36 -0800

    Initial commit

---
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -0,0 +1,30 @@
+package com.cydeo.tests.base;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        // 1. Open Chrome browser
+        //WebDriver driver = new ChromeDriver();
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+    }
+
+    @AfterMethod
+    public void teardown(){
+        //driver.quit();
+    }
+}
